1c17e6dc6a4de56e022d24f362a4ad1d70273117
Thomas McCarthy <thomasjmmccarthy@gmail.com>
2023-04-30 18:42:58 +0100

    Refactored to work with Conda VirtualEnvironment

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ import numpy as np
 dataset, dataset_info = tfds.load('oxford_flowers102', data_dir=(os.getcwd() + '/dataset'), with_info=True, as_supervised=True)
 test_set, training_set, validation_set = dataset['test'], dataset['train'], dataset['validation']
 
-print(len(training_set))
-
 #normalization_layer = keras.layers.Rescaling(1./255)
-
 #normalized_dataset = training_set.map(lambda x, y: (normalization_layer(x),y))
 #images, labels = next(iter(normalized_dataset))
 
@@ -35,26 +32,36 @@ test_batches = test_set.cache().map(format_image).batch(BATCH_SIZE).prefetch(1)
 #autotune = tf.data.AUTOTUNE
 #Snormalized_dataset = normalized_dataset.cache().prefetch(buffer_size=autotune)
 
+
+# Provide details on physical device(s) being used
+gpus = tf.config.list_physical_devices('GPU')
+print(("\n" + (40 * '-') + "\n\n") + "Number of GPUs Available: ", len(gpus))
+if(gpus): running_device = 'GPU'
+else: running_device = 'CPU'
+print("> Running on", running_device, ("\n\n" + (40 * '-') + "\n\n"))
+  
+
 num_classes = 102
 
-model = tf.keras.Sequential([
-  tf.keras.layers.Conv2D(32, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(64, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(96, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(96, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.22),
-  tf.keras.layers.Dense(num_classes)
+
+model = keras.Sequential([
+  keras.layers.Conv2D(32, 3, activation='relu'),
+  keras.layers.MaxPooling2D(),
+  keras.layers.Conv2D(64, 3, activation='relu'),
+  keras.layers.MaxPooling2D(),
+  keras.layers.Conv2D(96, 3, activation='relu'),
+  keras.layers.MaxPooling2D(),
+  keras.layers.Conv2D(96, 3, activation='relu'),
+  keras.layers.MaxPooling2D(),
+  keras.layers.Flatten(),
+  keras.layers.Dense(128, activation='relu'),
+  keras.layers.Dropout(0.22),
+  keras.layers.Dense(num_classes)
 ])
 
 model.compile(
   optimizer='adam',
-  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
   metrics=['accuracy'])
 
 model.fit(
@@ -62,3 +69,5 @@ model.fit(
   validation_data=validation_batches,
   epochs=6
 )
+
+print("Process finished")
